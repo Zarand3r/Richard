ae8cfa2ad3fa20ae1ab928aa4e6545f129680191
Richard Bao <richardbao419@gmail.com>
2018-08-13 00:57:56 -0400

    grid

--- js/slippery.js
+++ js/slippery.js
@@ -3,9 +3,14 @@ var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
+var windowWidth = $(window).width();
 
 $(window).scroll(function(event){
     didScroll = true;
+});
+
+$(window).on('resize', function(){
+      windowWidth = $(window).width();
 });
 
 setInterval(function() {
@@ -24,13 +29,19 @@ function hasScrolled() {
 
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
+    var navup = 'nav-up';
+    if (windowWidth < 800)  {
+      navup = 'nav-upper';
+    }
+
     if (st > lastScrollTop && st > navbarHeight){
         // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
+        $('header').removeClass('nav-down').addClass(navup);
+    }
+    else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
+            $('header').removeClass(navup).addClass('nav-down');
         }
     }
 
